c0727677a04a053e25fbd39770f41a78debbeb71
MD JUHAIAR MANAR CHOWDHURY <89413439+JuhairManar@users.noreply.github.com>
2025-05-14 23:10:34 +0600

    Create solution.cpp

---
+++ Codechef/Starters 186 (Rated)/Chess Win/solution.cpp
@@ -0,0 +1,12 @@
+// link - https://www.codechef.com/problems/CHESSWIN
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a,b;
+	cin>>a>>b;
+	cout<<b-a+1;
+
+}
