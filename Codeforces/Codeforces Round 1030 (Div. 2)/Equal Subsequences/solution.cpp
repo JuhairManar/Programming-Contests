9287e4e0732b29f11e3db134d2f81a5e4331269c
MD JUHAIAR MANAR CHOWDHURY <89413439+JuhairManar@users.noreply.github.com>
2025-06-12 23:47:20 +0600

    Create solution.cpp

---
+++ Codeforces/Codeforces Round 1030 (Div. 2)/Equal Subsequences/solution.cpp
@@ -0,0 +1,34 @@
+// link - https://codeforces.com/contest/2118/problem/0
+
+#pragma GCC optimize("Ofast")
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+
+void solve(){
+    int n,k,d,i;
+    cin>>n>>k;
+
+    for(i=0;i<k;i++) cout<<"1";
+    for(i;i<n;i++) cout<<"0";
+    cout<<"\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+/*  freopen(input.txt, 'r', stdin);
+    freopen(output.txt, 'w', stdout); */
+                                        
+    int t;
+    cin>>t;
+
+    while(t--){
+        solve();
+    }
+
+  return 0;
+}
