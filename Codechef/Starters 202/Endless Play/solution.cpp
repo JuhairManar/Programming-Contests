ff8a14a2beb331907a567cfc2b6beddbeaa2560c
MD JUHAIAR MANAR CHOWDHURY <89413439+JuhairManar@users.noreply.github.com>
2025-09-04 00:03:36 +0600

    Create solution.cpp

---
+++ Codechef/Starters 202/Endless Play/solution.cpp
@@ -0,0 +1,26 @@
+// link - https://www.codechef.com/problems/SKONG
+
+#pragma GCC optimize("Ofast")
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+
+void solve(){
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+/*  freopen(input.txt, 'r', stdin);
+    freopen(output.txt, 'w', stdout); */
+    int a,b,c,s;
+    cin>>a>>b;
+    int d=(a-4);
+    cout<<(d*24)+b;                                    
+
+
+  return 0;
+}
