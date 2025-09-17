ae28dc76185fd77535ce6dd0df49e4557d9c6d19
MD JUHAIAR MANAR CHOWDHURY <89413439+JuhairManar@users.noreply.github.com>
2025-09-17 23:59:58 +0600

    Create solution.cpp

---
+++ Codechef/Starters 204/Drawing Chances/solution.cpp
@@ -0,0 +1,25 @@
+//
+
+#pragma GCC optimize("Ofast")
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+
+void solve(){
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+/*  freopen(input.txt, 'r', stdin);
+    freopen(output.txt, 'w', stdout); */
+    int a,b;
+    cin>>a>>b;
+    cout<<180-(a+b)<<"\n";                                    
+
+
+  return 0;
+}
