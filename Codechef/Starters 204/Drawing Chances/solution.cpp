a479c953cec73935bf731a9825314e27a25e6c97
MD JUHAIAR MANAR CHOWDHURY <89413439+JuhairManar@users.noreply.github.com>
2025-09-18 00:00:57 +0600

    Update solution.cpp

--- Codechef/Starters 204/Drawing Chances/solution.cpp
+++ Codechef/Starters 204/Drawing Chances/solution.cpp
@@ -1,4 +1,5 @@
-//
+// link - https://www.codechef.com/START204D/problems/DRAWCH
+
 
 #pragma GCC optimize("Ofast")
 #include<bits/stdc++.h>
@@ -8,6 +9,22 @@ using namespace std;
 #define pb push_back
 
 void solve(){
+    int n,m,a=0,b=0,dp,dm;
+    cin>>n>>m;
+    string s;
+    cin>>s;
+    for(auto g:s){
+        if(g=='1') a++;
+        else b++;
+    }
+    dm=n-m;
+    dp=abs(a-b);
+
+    if(dm>=dp && (dm-dp)%2==0) {
+        cout<<"Yes\n";
+    }
+    else cout<<"No\n";
+
 }
 
 int main()
@@ -16,9 +33,13 @@ int main()
     cin.tie(NULL);
 /*  freopen(input.txt, 'r', stdin);
     freopen(output.txt, 'w', stdout); */
-    int a,b;
-    cin>>a>>b;
-    cout<<180-(a+b)<<"\n";                                    
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        solve();
+    }
+                                        
 
 
   return 0;
